a93d388c56ca313d2c50859152e0c0a2b3fd8268
Ashley Taylor <ashtaylor2010@gmail.com>
2024-08-20 10:00:54 +0100

    update env vars

--- api/database/database.py
+++ api/database/database.py
@@ -1,11 +1,15 @@
 import os
+from pathlib import Path
 
-import dotenv
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-dotenv.load_dotenv()
+CURRENT_DIR = Path(__file__).resolve().parent
+ENV_PATH = CURRENT_DIR.parent.parent / ".env"
+
+load_dotenv(dotenv_path=ENV_PATH)
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DB_POSTGRES_URL")
 
